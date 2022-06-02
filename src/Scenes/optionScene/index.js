f7da55bfe3f35b0ca0be816a241ab3b909688b08
BlueDream0718 <104906327+Cheng0718@users.noreply.github.com>
2022-06-01 16:57:01 -0800

    vv

--- src/Scenes/optionScene/index.js
+++ src/Scenes/optionScene/index.js
@@ -230,12 +230,27 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo, continueS
         answerList = []
         let needLength = optionGroup[stepCount];
 
-        while (answerList.length != needLength) {
-            let randomNumber = Math.floor(Math.random() * needLength);
-            if (!answerList.includes(doneCount + randomNumber)) {
-                answerList.push(doneCount + randomNumber)
-            }
-        }
+        const defaultRandomList = [
+            [
+                [1, 0]
+            ],
+            [
+                [2, 0, 1], [1, 2, 0]
+            ],
+            [
+                [4, 3, 2, 1], [4, 3, 1, 2],
+                [2, 1, 4, 3], [2, 1, 3, 4],
+                [3, 4, 2, 1], [3, 4, 1, 2]
+            ]
+        ]
+
+        let currentNum = needLength - 2
+
+        let randomNumber = Math.floor(Math.random() * defaultRandomList[currentNum].length);
+
+        defaultRandomList[currentNum][randomNumber].map(value => {
+            answerList.push(value + doneCount)
+        })
     }
 
     if (answerList.length == 0)
@@ -258,9 +273,9 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo, continueS
 
             if (correctNum == answerList.length) {
                 setTimeout(() => {
-                    goNextStep()    
+                    goNextStep()
                 }, 1000);
-                
+
             }
 
             else {

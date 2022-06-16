0ff500ceabbc7e3defff08f8211c8adb26d2fce4
BlueDream0718 <104906327+Cheng0718@users.noreply.github.com>
2022-06-16 06:02:38 -0800

    v111

--- src/Scenes/optionScene/index.js
+++ src/Scenes/optionScene/index.js
@@ -88,12 +88,9 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo, continueS
                 audioList.bodyAudio1.play();
                 timerList[1] = setTimeout(() => {
                     audioList.bodyAudio2.play();
-                    timerList[2] = setTimeout(() => {
-                        startRepeatAudio()
-                        // audioList.commonAudio1.play();
-                    }, audioList.bodyAudio2.duration * 1000 + 300);
+                    startRepeatAudio()
                 }, audioList.bodyAudio1.duration * 1000 + 300);
-            }, 1500);
+            }, 2000);
         },
         startGame: () => {
 
@@ -114,7 +111,7 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo, continueS
                         audioList.commonAudio1.play();
                     }, audioList.bodyAudio2.duration * 1000 + 300);
                 }, audioList.bodyAudio1.duration * 1000 + 600);
-            }, 1500);
+            }, 2000);
         }
     }))
 
@@ -206,12 +203,9 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo, continueS
                             audioList.bodyAudio1.play();
                             timerList[1] = setTimeout(() => {
                                 audioList.bodyAudio2.play();
-                                timerList[2] = setTimeout(() => {
-                                    startRepeatAudio()
-                                    // audioList.commonAudio1.play();
-                                }, audioList.bodyAudio2.duration * 1000 + 300);
+                                startRepeatAudio()
                             }, audioList.bodyAudio1.duration * 1000 + 300);
-                        }, 1500);
+                        }, 2000);
                     }
                 }, waitTime);
             }
@@ -287,10 +281,7 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo, continueS
                     parentObject.current.style.pointerEvents = ''
 
                     audioList.bodyAudio2.play();
-                    timerList[2] = setTimeout(() => {
-                        startRepeatAudio()
-                        // audioList.commonAudio1.play();
-                    }, audioList.bodyAudio2.duration * 1000 + 300);
+                    startRepeatAudio()
                 }, 2000);
             }
         }
@@ -301,9 +292,9 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo, continueS
             timerList[1] = setTimeout(() => {
                 audioList.bodyAudio2.currentTime = 0
                 audioList.bodyAudio2.play();
-                timerList[2] = setTimeout(() => {
-                    startRepeatAudio()
-                }, audioList.bodyAudio2.duration * 1000);
+
+                startRepeatAudio()
+
             }, 1000);
         }
     }
